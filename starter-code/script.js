e8457101f5f0b710ee176cfb551cf086dc29a104
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-16 10:15:59 +0200

    add lowercase abc

--- starter-code/script.js
+++ starter-code/script.js
@@ -2,9 +2,15 @@ const btn = document.getElementById("btn")
 const abc = ["a","b","c"]
 const abcLength = abc.length
 
+/* abc */
+const lowercaseLetters = Array.from({length: 26}, (_, index) => String.fromCharCode(97 + index));
+console.log(lowercaseLetters);
+
+
 
 // make a func which 
 btn.addEventListener("click",()=>{
     const randNumber = Math.floor(Math.random()*abcLength)
     console.log(randNumber)
+    console.log(abc[randNumber])
 })
