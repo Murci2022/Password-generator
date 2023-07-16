a7de054e0033688314e9901519e37efc5f7185aa
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-16 10:26:14 +0200

    add logic to generate 3 rand letter from abc

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,16 +1,20 @@
 const btn = document.getElementById("btn")
-const abc = ["a","b","c"]
-const abcLength = abc.length
+
 
 /* abc */
 const lowercaseLetters = Array.from({length: 26}, (_, index) => String.fromCharCode(97 + index));
-console.log(lowercaseLetters);
+console.log(typeof lowercaseLetters)
+const abcLength = lowercaseLetters.length
+console.log(abcLength)
 
 
 
 // make a func which 
 btn.addEventListener("click",()=>{
-    const randNumber = Math.floor(Math.random()*abcLength)
-    console.log(randNumber)
-    console.log(abc[randNumber])
+    const randNumber1 = Math.floor(Math.random()*abcLength)
+    const randNumber2 = Math.floor(Math.random()*abcLength)
+    const randNumber3 = Math.floor(Math.random()*abcLength)
+    const password = lowercaseLetters[randNumber1]+lowercaseLetters[randNumber2]+lowercaseLetters[randNumber3]
+    console.log(password)
+
 })
