ed8ba052b78c7feaf241e7e27c5df4a442bf5fb9
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-17 10:00:46 +0200

    generate password function for lowerCase, add btn.click func

--- starter-code/index.js
+++ starter-code/index.js
@@ -3,6 +3,8 @@ const displayPassword = document.getElementById("header-title")
 const checkBox1 = document.getElementById("checkbox1");
 const checkBox2 = document.getElementById("checkbox2");
 const checkBox3 = document.getElementById("checkbox3");
+/* initial password */
+let password = [];
 
 
 
@@ -27,6 +29,7 @@ const abcLength = lowercaseLetters.length;
 const allNrLength = allNr.length;
 
 
+
 // generate random index function
 function randIndex(array){
     return Math.floor(Math.random()*array.length)
@@ -36,6 +39,24 @@ function randIndex(array){
 
 console.log(randIndex(allNr))
 
+
+
+
 // generate password function
+function generatePassword(){
+    const passwordLetter = [
+        lowercaseLetters[randIndex(lowercaseLetters)],
+        lowercaseLetters[randIndex(lowercaseLetters)],
+        lowercaseLetters[randIndex(lowercaseLetters)],
+]
+    password = passwordLetter.join("")
+     
+
+    return password;
+}
+
 
 // click event on btn
+btn.addEventListener("click",()=>{
+    console.log(generatePassword())
+})
