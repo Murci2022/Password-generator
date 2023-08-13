d9cc95ab7562e381c5ea70e4f8cf78d697f622d1
MK <Javascript@mates-air.localdomain>
2023-08-13 15:12:38 +0200

    cleaning up my code and one more correction if none of the checkboxes is checked

--- index.js
+++ index.js
@@ -1,288 +1,219 @@
-const btn = document.getElementById("btn")
-const displayPassword = document.getElementById("header-title")
-const checkBox1 = document.getElementById("checkbox1");
-const checkBox2 = document.getElementById("checkbox2");
-const checkBox3 = document.getElementById("checkbox3");
-const checkBox4 = document.getElementById("checkbox4");
-const nrConti = document.getElementById("nr")
-const copyBtn = document.getElementById("header-icon")
-const password = document.getElementById("header-title")
-const svgTitle = document.getElementById("svg-title")
-const resetBtn = document.getElementById("btn-reset")
-const msg = document.getElementById("msg")
-const msgReset = document.getElementById("msg-reset")
+const btn = document.getElementById('btn');
+const displayPassword = document.getElementById('header-title');
+const checkBox1 = document.getElementById('checkbox1');
+const checkBox2 = document.getElementById('checkbox2');
+const checkBox3 = document.getElementById('checkbox3');
+const checkBox4 = document.getElementById('checkbox4');
+const nrConti = document.getElementById('nr');
+const copyBtn = document.getElementById('header-icon');
+const password = document.getElementById('header-title');
+const svgTitle = document.getElementById('svg-title');
+const resetBtn = document.getElementById('btn-reset');
+const msg = document.getElementById('msg');
+const msgReset = document.getElementById('msg-reset');
 
-const slider = document.getElementById("slider");
+const slider = document.getElementById('slider');
 
-/* initial  */
 let displayNr = 8;
 
-
-
-
-
-
-
-/* abc LOWER*/
-const lowercaseLetters = Array.from({length: 26}, (_, index) => String.fromCharCode(97 + index));
-
-
-/* abc UPPER */
-const uppercaseLetters = Array.from({ length: 26 }, (_, index) =>
-String.fromCharCode(65 + index)
+const lowercaseLetters = Array.from({ length: 26 }, (_, index) =>
+  String.fromCharCode(97 + index)
 );
 
-/* nr */
-const allNr = [0,1,2,3,4,5,6,7,8,9];
+const uppercaseLetters = Array.from({ length: 26 }, (_, index) =>
+  String.fromCharCode(65 + index)
+);
 
-/* symbols */
-const allSymbols = [
-    "!", "@", "#", "$", "%",  "&", "*","+",
-  ];
-  
+const allNr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-/* length */
+const allSymbols = ['!', '@', '#', '$', '%', '&', '*', '+'];
+
 const abcLength = lowercaseLetters.length;
 const allNrLength = allNr.length;
 
-
-/* function slide */
-
-
-
-
-//------------------------------------- generate random index function
-function randIndex(array){
-    return Math.floor(Math.random()*array.length)
-};
-
-
-/* funcTEst */
-
-
-function testFunc(test){
-    console.log(test,"test")
+function randIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
 
+function generatePassword() {
+  let password = '';
 
+  if (checkBox1.checked) {
+    const indexes = [
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+      randIndex(lowercaseLetters),
+    ];
+    password += indexes.map((index) => lowercaseLetters[index]).join('');
+  }
 
-// -------------------------------------generate password function
-function generatePassword(){
+  if (checkBox2.checked) {
+    const indexes = [
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+      randIndex(uppercaseLetters),
+    ];
+    password += indexes.map((index) => uppercaseLetters[index]).join('');
+  }
+  if (checkBox3.checked) {
+    const indexes = [
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+      randIndex(allNr),
+    ];
+    password += indexes.map((index) => allNr[index]).join('');
+  }
+  if (checkBox4.checked) {
+    const indexes = [
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+      randIndex(allSymbols),
+    ];
+    password += indexes.map((index) => allSymbols[index]).join('');
+  }
 
-    
-    let password = "";
-    
-    if(checkBox1.checked){
-        const indexes = [
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-            randIndex(lowercaseLetters),
-        ];
-        password += indexes.map((index)=>lowercaseLetters[index]).join("")
-
-        
-    }
-    
-    if(checkBox2.checked){
-        const indexes = [
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-            randIndex(uppercaseLetters),
-        ];
-        password += indexes.map((index)=>uppercaseLetters[index]).join("")
-       
-    }
-    if(checkBox3.checked){
-        const indexes = [
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-            randIndex(allNr),
-        ];
-        password += indexes.map((index)=>allNr[index]).join("")
-        
-    }
-    if(checkBox4.checked){
-        const indexes = [
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-            randIndex(allSymbols),
-        ];
-        password += indexes.map((index)=>allSymbols[index]).join("")
-       
-    }
-    
-    
-    return password.slice();
-    
-    
-    
-    
+  return password.slice();
 }
 
-/* -----------Fisher-Yates Shuffle---------------- */
+function shuffleFunc(fullArr) {
+  const newDisplayNr = Number(displayNr);
 
-function shuffleFunc(fullArr){
-    const newDisplayNr = Number(displayNr);
-    console.log(typeof newDisplayNr)
-    
-    console.log(fullArr,"fullArr")
-    
-    const shuffledArray = [...fullArr]
-    console.log(shuffledArray,"shuffledArr")
+  const shuffledArray = [...fullArr];
+  console.log(shuffledArray, 'shuffledArr');
 
-    let temp = ""
-    let i = fullArr.length
-    
-    while(--i>=0){
-        const j = Math.floor(Math.random()*(shuffledArray.length))
-        temp = shuffledArray[j]
-        shuffledArray[j]=shuffledArray[i]
-        shuffledArray[i]=temp;
-        
+  let temp = '';
+  let i = fullArr.length;
+
+  while (--i >= 0) {
+    const j = Math.floor(Math.random() * shuffledArray.length);
+    temp = shuffledArray[j];
+    shuffledArray[j] = shuffledArray[i];
+    shuffledArray[i] = temp;
+  }
+  return shuffledArray.join('').slice(0, newDisplayNr);
 }
-return shuffledArray.join("").slice(0,newDisplayNr)
-}
-
-
-
-
-
-
-
 
 // click event on btn
-btn.addEventListener("click",()=>{
-    svgTitle.innerHTML = "copy to clipboard";
-    password.style.visibility= "visible";
-    if(checkBox1.checked || checkBox2 || checkBox3 || checkBox4){
-        
-        const endArr = shuffleFunc(generatePassword())
-        
-        const fullShaffledPassword = endArr ;
-        
-        
-        displayPassword.innerHTML = fullShaffledPassword;
-        
-    }else{
-        console.log("you have to check one of the boxes")
-    }
-})
+btn.addEventListener('click', () => {
+  svgTitle.innerHTML = 'copy to clipboard';
+  password.style.visibility = 'visible';
+  if (
+    checkBox1.checked ||
+    checkBox2.checked ||
+    checkBox3.checked ||
+    checkBox4.checked
+  ) {
+    const endArr = shuffleFunc(generatePassword());
+
+    const fullShaffledPassword = endArr;
+
+    displayPassword.innerHTML = fullShaffledPassword;
+  } else {
+    displayPassword.innerHTML = 'Please select an option';
+  }
+});
 
 /* slider change */
 
-slider.addEventListener("change",(e)=>{
-    console.log(e.target.value,"posi")
-    displayNr = e.target.value;
-    testFunc(displayNr)
-    
-    
-    nrConti.innerHTML = displayNr;
-    
-    
-})
+slider.addEventListener('change', (e) => {
+  console.log(e.target.value, 'posi');
+  displayNr = e.target.value;
+  testFunc(displayNr);
+
+  nrConti.innerHTML = displayNr;
+});
 
 /* function copyToClipboard */
-function copyToClipboard(){
-    console.log(password.innerText,"copyToClipboard")
-    const copyText = password.innerText;
-    navigator.clipboard.writeText(copyText).then(()=>{
-        console.log(copyText,"copied")
 
-        setTimeout(()=>{
-            svgTitle.innerHTML = "copy to clipboard"
-        },4400)
-    })
+function copyToClipboard() {
+  console.log(password.innerText, 'copyToClipboard');
+  const copyText = password.innerText;
+  navigator.clipboard.writeText(copyText).then(() => {
+    console.log(copyText, 'copied');
+
+    setTimeout(() => {
+      svgTitle.innerHTML = 'copy to clipboard';
+    }, 4400);
+  });
 }
 
 /*  copy button */
 
-copyBtn.addEventListener("click",()=>{
-    // display msg
-    msg.style.opacity = "1"
+copyBtn.addEventListener('click', () => {
+  // display msg
+  msg.style.opacity = '1';
 
-    setTimeout(()=>{
-        msg.style.opacity = "0"
-    },1800)
+  setTimeout(() => {
+    msg.style.opacity = '0';
+  }, 1800);
 
-   
-    
-    copyToClipboard();
-    
-
-})
+  copyToClipboard();
+});
 
 /* reset btn */
 
-resetBtn.addEventListener("click",()=>{
+resetBtn.addEventListener('click', () => {
+  //checkbox reset
+  checkBox1.checked = true;
+  checkBox2.checked = false;
+  checkBox3.checked = false;
+  checkBox4.checked = false;
 
+  // number reset
+  displayNr = 8;
+  nrConti.innerHTML = displayNr;
 
-    //checkbox reset
-    checkBox1.checked = true;
-    checkBox2.checked = false;
-    checkBox3.checked = false;
-    checkBox4.checked = false;
+  //slider reset
+  console.log(slider.value, 'sliderValue');
+  slider.value = displayNr;
 
-    // number reset
-    displayNr = 8;
-    nrConti.innerHTML = displayNr;
+  //reseted@
+  password.style.visibility = 'hidden';
 
-    //slider reset
-    console.log(slider.value,"sliderValue")
-    slider.value = displayNr
+  msgReset.style.opacity = '1';
+  console.log(msgReset);
 
-    //reseted@
-    password.style.visibility= "hidden"
-   
-     msgReset.style.opacity="1"
-    console.log(msgReset)
+  setTimeout(() => {
+    msgReset.style.opacity = '0';
+  }, 2000);
 
-    setTimeout(()=>{
-        msgReset.style.opacity = "0" 
-    },2000)
-   
+  console.log(nrConti.innerText);
 
-    
-  
-    console.log("click")
-    console.log(nrConti.innerText)
-    
-    console.log(checkBox2)
-})
-
-
+  console.log(checkBox2);
+});
